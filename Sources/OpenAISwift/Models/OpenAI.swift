94d84bc4d1070b6b8a121d691c5315d1bb28ef0d
ChiFung <chifung.he@gmail.com>
2023-03-02 13:32:38 +0800

    update

--- Sources/OpenAISwift/Models/OpenAI.swift
+++ Sources/OpenAISwift/Models/OpenAI.swift
@@ -11,6 +11,6 @@ public struct OpenAI: Codable {
 }
 
 public struct Choice: Codable {
-    public let text: String
-    public let message: ChatMessage
+    public let text: String?
+    public let message: ChatMessage?
 }
